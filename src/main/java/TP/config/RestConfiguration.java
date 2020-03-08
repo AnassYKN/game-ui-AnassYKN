a78a6852009fb524dfec57adb316321021446cc7
Anass YAKINE <anassyakine06@gmail.com>
2020-03-08 02:42:02 +0100

    [WIP] add RestConfiguration

--- src/main/java/TP/config/RestConfiguration.java
+++ src/main/java/TP/config/RestConfiguration.java
@@ -1,12 +1,27 @@
 package TP.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestConfiguration {
+
+    private String username="user";
+    private String password="787f7228-60da-11ea-bc55-0242ac130003";
+
     @Bean
-    RestTemplate restTemplate(){
+    public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+    @Bean
+    public RestTemplate trainerApiRestTemplate(){
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(new BasicAuthenticationInterceptor(username,password));
+        return restTemplate;
+    }
+
+
 }
